ffd07c9051687313b647390915d5896a01739aeb
petardev101 <workdev1989@gmail.com>
2017-12-12 08:17:39 -0600

    dont abbreviate rule

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Simple Rules for Simple Code
 
+    1. Don't abbreviate your code, but don't exceed two words (aka no $thisIsACoolVariable, rather $coolVariable)
+    2.  Never use the `else` keyword.
--- index.php
+++ index.php
@@ -1,2 +1,33 @@
 <?php
 
+//never abbreviate!
+class Trnsltr
+{
+
+}
+
+//adds up quickly to a poorly built app. $x for name is difficult to remember
+foreach ( $x in $people )
+{
+
+}
+
+//be explicit and use UserRepository
+class UserRepo
+{
+    public function fetch($billingId) //change fetch to fetchByBillingId
+    {
+        
+    }
+}
+
+//be explicit, but not too explicit
+class Order
+{
+    public function shipOrder()
+    {
+        
+    }
+}
+
+$order->shipOrder(); //could change method to ship to avoid repeating order
